ac2149e15ee1c52a4f0e98324c99d10f299dde41
Mohamed Yassine <lhachimimedyassine@gmail.com>
2025-09-04 20:19:47 +0100

    preloader fade fix

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,3 +1,6 @@
 window.addEventListener('load',function(){
-  document.querySelector('.preloader').classList.add("hidden")  
+  document.querySelector('.preloader').classList.add("fade-out")
+  setTimeout(()=> {
+    document.querySelector('.preloader').classList.add("hidden")
+  }, 800)
 });
