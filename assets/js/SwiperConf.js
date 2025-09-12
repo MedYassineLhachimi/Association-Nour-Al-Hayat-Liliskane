c3d27ab7bd168858fd86efea9ce62c279c23a6f9
Mohamed Yassine <lhachimimedyassine@gmail.com>
2025-09-12 17:38:37 +0100

    slider

---
+++ assets/js/SwiperConf.js
@@ -0,0 +1,30 @@
+
+let swiper = new Swiper (".swiper", { effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 4,
+  loop: true,
+  speed: 600,
+  preventClicks: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100, 
+    modifier: 2.5,
+    slideShadows: false,
+  },
+  on: {
+    click(event) {
+    swiper.slideTo(this.clickedIndex);
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
